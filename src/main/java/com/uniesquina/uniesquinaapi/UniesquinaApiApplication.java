0352fde0b4b45dbc09ed3f8f62aee003e6e04f74
mauriciocoruja <mbjr@outlook.com.br>
2021-09-05 15:54:13 -0300

    Pasta do projeto

---
+++ src/main/java/com/uniesquina/uniesquinaapi/UniesquinaApiApplication.java
@@ -0,0 +1,13 @@
+package com.uniesquina.uniesquinaapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniesquinaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UniesquinaApiApplication.class, args);
+	}
+
+}
